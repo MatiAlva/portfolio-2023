7cef8037efc1f84b8ae22fab9eec5a343a2b162e
MatiAlva <83080975+MatiAlva@users.noreply.github.com>
2023-02-21 16:58:50 -0300

    :white_check_mark: Retoques grandes terminado

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from 'react'
-import TextTags from './textTags'
 import { AnimatedLetters } from '../AnimatedLetters'
 import Loader from 'react-loaders'
+import { faCss3, faGitAlt, faGithub, faHtml5, faJsSquare, faNodeJs, faReact, faSass } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './index.scss'
+
 
 export const Skills = () => {
 
@@ -12,6 +15,8 @@ export const Skills = () => {
             setLetterClass('text-animate-hover')
         }, 4000)
     }, [])
+
+
 
     return (
         <>
@@ -25,13 +30,58 @@ export const Skills = () => {
                         />
                     </h1>
                     <p>
-                        Soy Técnico Programador y Desarrollador Front End con amplios conocimientos y experiencia como Desarrollador Full Stack.
-                        Soy un gran trabajador en equipo capaz de aprender y aplicar rápidamente las nuevas tecnologías.
+                        Experto en desarrollo front-end incluyendo tecnologias como :
+                        <span className='yellow'> HTML5, SASS, CSS, REACT, Styled-Components, JavaScript,
+                            Bootstrap, Git, JQUEY.
+                        </span>
+                    </p>
+                    <p>Tambien tengo conocimientos y experiencia en lengujes de back-end como :
+                        <span className='yellow'> PHP, NODEJS, EXPRES, JWT, MYSQL, MONGODB,
+                            JEST, JAVA.
+                        </span>
+                    </p>
+                    <p>
+                        Visita mi perfil de <a
+                            href='https://www.linkedin.com/in/mati-dev/'
+                            target='_blank'
+                            className='yellow'>
+                            LinkedIn
+                        </a> para mas detalles. Tambien puede consultar mi CV <a
+                            href='/CV/Alvarez-Matias.pdf'
+                            download={true}
+                            className='yellow'
+                        >
+                            en este enlace</a> o sientase libre de echar un
+                        vistazo a algunos de mis proyectos en mi <a
+                            href='https://github.com/MatiAlva'
+                            target='_blank'
+                            className='yellow'>
+                            Github
+                        </a>.
                     </p>
                 </div>
 
-                <div className='container-animation'>
-                    <TextTags />
+                <div className="stage-cube-cont">
+                    <div className="cubespinner">
+                        <div className="face1">
+                            <FontAwesomeIcon icon={faNodeJs} color="#35b62e" />
+                        </div>
+                        <div className="face2">
+                            <FontAwesomeIcon icon={faGithub} color="#0c0c0c" />
+                        </div>
+                        <div className="face3">
+                            <FontAwesomeIcon icon={faSass} color="#a83173" />
+                        </div>
+                        <div className="face4">
+                            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+                        </div>
+                        <div className="face5">
+                            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+                        </div>
+                        <div className="face6">
+                            <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+                        </div>
+                    </div>
                 </div>
             </div>
             <Loader type='ball-pulse' />
