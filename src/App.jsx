7a2aa7b9bd2d07e9da7156003220265c984b1445
MatiAlva <83080975+MatiAlva@users.noreply.github.com>
2023-02-21 13:47:40 -0300

    Problemas con la animacion del About en deploy

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,10 @@ function App() {
     <>
       <Routes>
         <Route path='/' element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path='about' element={<About />} />
-          <Route path='skills' element={<Skills />} />
-          <Route path='projects' element={<Projects />} />
+          <Route index path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/skills' element={<Skills />} />
+          <Route path='/projects' element={<Projects />} />
           <Route path='/*' element={<Home />} />
         </Route>
       </Routes>
